a5b1c77b820e6581ceeb35d30132638401fb3825
staffordc <stafford.carh@gmail.com>
2018-08-30 21:36:19 -0400

    Filter by Categories

--- Website/app.js
+++ Website/app.js
@@ -1,8 +1,35 @@
+var myMovieJson = [];
 fetch('http://localhost:62268/api/Movies')
   .then(function(response) {
     return response.json();
   })
   .then(function(myJson) {
+    myMovieJson=myJson
+    $('.ui.dropdown')
+    .dropdown({
+      onChange: function (val) {
+          console.log(val);
+    var body = document.getElementById('MovieTableBody')
+    body.innerHTML = " ";
+    var filteredMovies = _.filter(myMovieJson, {'Category': val})
+    _.each(filteredMovies, function(Movie){
+        body.innerHTML += `
+        <tr>
+        <td>${Movie.Title}</td>
+        <td>${Movie.Category}</td>
+        <td>${Movie.Description}</td>
+      </tr>
+        `
+    })
+      },
+      values: _
+      .chain(myJson).uniqBy('Category')
+      .map(function(Movie){
+          return {name:Movie.Category, value:Movie.Category, text:Movie.Category}
+      }).value() 
+      
+    })
+  ;
     var body = document.getElementById('MovieTableBody')
     body.innerHTML = " ";
     _.each(myJson, function(Movie){
